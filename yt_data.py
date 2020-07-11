6de1844a183d807e5678118eac73d239966b7fdb
erzz99 <erskinengugi99@gmail.com>
2020-07-11 16:22:40 -0500

    search results

--- yt_data.py
+++ yt_data.py
@@ -1,3 +1,16 @@
 from googleapiclient.discovery import build
 
 api_key = 'AIzaSyCsTGj90PwHrlmC8k37EHoZTlqhZJXQddA'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.search().list(
+    part='snippet',
+    q='soccer vanderbilt',
+    maxResults=100,
+
+)
+
+response = request.execute()
+
+print(response)

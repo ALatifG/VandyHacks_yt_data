be091dd93ea6d4ed9b70941907a3081e4424a5cc
Abdul-Latif G <abdullatifgbada@gmail.com>
2020-07-11 16:28:43 -0500

    ..

--- yt_data.py
+++ yt_data.py
@@ -1,3 +1,22 @@
 from googleapiclient.discovery import build
+import json
 
 api_key = 'AIzaSyCsTGj90PwHrlmC8k37EHoZTlqhZJXQddA'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part = 'statistics',
+    forUsername = "TechGuyWeb"
+)
+
+response = request.execute()
+# print(response)
+
+search = youtube.search().list(
+    part = 'snippet'
+    q = 
+)
+
+search_response = search.execute()
+search_js = json.loads(search_response)
